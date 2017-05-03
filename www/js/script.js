a2258d3495dc3f809449914b68944c2935b7a76d
zubayer-rogue <haider15-5902@diu.edu.bd>
2017-05-03 17:06:47 +0600

    90 Done

--- www/js/script.js
+++ www/js/script.js
@@ -1,3 +1,42 @@
+$.fn.counter = function( options ) {
+    var settings = $.extend({
+        start:  0,
+        end:    100,
+        easing: 'swing',
+        duration: 1000,
+        complete: ''
+    }, options );
+
+    var el = $(this);
+
+    $({count: settings.start}).animate({count: settings.end}, {
+		duration: settings.duration,
+		easing: settings.easing,
+		step: function() {
+			var mathCount = Math.ceil(this.count);
+			el.text(mathCount);
+		},
+		complete: settings.complete
+	});
+};
+
+function scrolled(el) {
+}
 jQuery(document).ready(function($){
     $('.content-wrap').css('margin-bottom', $('footer').height());
+
+    $(window).scroll(function() {
+        $('section:not(.animate)').each(function(){
+            if( $(window).scrollTop() + 300  >= $(this).position().top){
+                $(this).addClass('animate').find('.counter').queue(function(){
+                    val = $(this).data('counter');
+                    $(this).counter({end: val, duration: 1000});
+                });
+            }
+        });
+    });
+    $('a.slider-arrow').click(function(event) {
+        event.preventDefault();
+        $('body').animate({ scrollTop: $('#Slider').height() }, 1000);
+    });
 });
