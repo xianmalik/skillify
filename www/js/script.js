4b6b34057570a73ebf3fd9cf7392461e3d1024ec
zubayer-rogue <haider15-5902@diu.edu.bd>
2017-05-03 12:43:18 +0600

    Drop Shadow

--- www/js/script.js
+++ www/js/script.js
@@ -1,21 +1,3 @@
-angular.module("navRouting", ["ngRoute"])
-
-    .config(function($routeProvider, $locationProvider) {
-
-        $routeProvider
-            .when("/", {
-                templateUrl : "templates/home.htm"
-            })
-            .when("/mentors", {
-                templateUrl : "templates/mentors.htm"
-            })
-            .otherwise({
-                redirectTo: "/"
-            });
-
-        // use the HTML5 History API
-        $locationProvider.hashPrefix('').html5Mode({
-            enabled: true,
-            requireBase: false
-        });
-    });
+jQuery(document).ready(function($){
+    $('.content-wrap').css('margin-bottom', $('footer').height());
+});
